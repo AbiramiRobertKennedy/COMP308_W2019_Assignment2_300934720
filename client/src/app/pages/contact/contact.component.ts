907f5e254f810c35eb67ed649433846353d0dbf9
Abirami <abiramirobertkennedy@gmail.com>
2019-03-27 18:14:45 -0700

    Updated pages and partials

---
+++ client/src/app/pages/contact/contact.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BasePageComponent } from 'src/app/partials/base-page/base-page.component';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent extends BasePageComponent implements OnInit {
+
+  constructor(
+    route: ActivatedRoute,
+    //private authService: AuthService
+    ) {
+    super(route);
+  }
+
+  ngOnInit() {
+  }
+
+  //isLoggedIn(): boolean {
+   // return this.authService.loggedIn();
+  }
+
+//}
