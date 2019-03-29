1a027c88b442f6aae651b0cf172a96ad6c7e0f95
Abirami <abiramirobertkennedy@gmail.com>
2019-03-29 06:56:34 -0700

    Added add and edit components

--- client/src/app/services/contact-list.service.ts
+++ client/src/app/services/contact-list.service.ts
@@ -3,11 +3,13 @@ import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Contact } from '../models/contact';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactListService {
+  private user: User;
   private endpoint = 'http://localhost:3000/api/contact-list/';
   private httpOptions = {
     headers: new HttpHeaders({
@@ -18,10 +20,27 @@ export class ContactListService {
   };
 
   constructor(private http: HttpClient) {
+    this.user = new User();
   }
 
   public getList(): Observable<any> {
     return this.http.get<any>(this.endpoint, this.httpOptions);
+  }
+
+  public getContact(contact: Contact): Observable<any> {
+    return this.http.get<any>(this.endpoint + 'edit/' + contact._id, this.httpOptions);
+  }
+
+  public addContact(contact: Contact): Observable<any> {
+    return this.http.post<any>(this.endpoint + 'add', contact, this.httpOptions);
+  }
+
+  public editContact(contact: Contact): Observable<any> {
+    return this.http.post<any>(this.endpoint + 'edit/' + contact._id, contact, this.httpOptions);
+  }
+
+  public deleteContact(contact: Contact): Observable<any> {
+    return this.http.get<any>(this.endpoint + 'delete/' + contact._id, this.httpOptions);
   }
 
 }
